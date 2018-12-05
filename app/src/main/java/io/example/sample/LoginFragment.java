e6d54d6bfac24e4b447ea705d899b164890cc638
Gurupad Mamadapur <gurupadmamadapur@gmail.com>
2018-12-05 21:47:55 +0530

    Setup kotlin

--- app/src/main/java/io/example/sample/LoginFragment.java
+++ app/src/main/java/io/example/sample/LoginFragment.java
@@ -1,5 +1,6 @@
 package io.example.sample;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import io.example.sample.databinding.FragmentLoginBinding;
+
 public class LoginFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        FragmentLoginBinding binding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        return binding.getRoot();
     }
 }
